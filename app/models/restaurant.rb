fe646511ec5d43961e96602825fe066abd155f9b
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2021-08-10 12:07:48 +0400

    crud views with bootstrap, validations for restarurant model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,6 @@
 class Restaurant < ApplicationRecord
   validates :name, presence: true
   validates :address, presence: true
-  validates :category, presence: true
+  CATEGORIES = %w[chinese italian japanese french belgian].freeze
+  validates :category, presence: true, inclusion: { in: CATEGORIES }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: "+40 52 333 3333", category: "french"}
+pizza_east = { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number: "+44 52 333 3333", category: "belgian"}
+zuma = { name: "Zuma", address: "DIFC, Dubai", phone_number: "+971 52 333 3333", category: "japanese"}
+
+[ dishoom, pizza_east, zuma ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+
+puts "Finished!"

aed59fcd3aa5c7b2a5ad98576ee8b6d8d3f13a3f
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2021-08-10 12:38:43 +0400

    generate a review model with reference to restaurant model

---
+++ app/models/review.rb
@@ -0,0 +1,7 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+
+  validates :content, presence: true, length: { minimum: 5 }
+  RATINGS = [0, 1, 2, 3, 4, 5].freeze
+  validates :rating, presence: true, inclusion: { in: RATINGS }, numericality: true
+end
